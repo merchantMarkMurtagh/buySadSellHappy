58dc0ef0bb8df5630adf55a89e99999bc0293f97
Mark Murtagh <mamurtag@tcd.ie>
2018-02-18 18:32:51 +0000

    hide api keys

--- bittrex/scraper.py
+++ bittrex/scraper.py
@@ -5,10 +5,10 @@ from bittrex.bittrex import Bittrex
 import bittrexAPI as btx
 
 
-consumer_key = '1Z0mxhdSBmRoS6FPE04VRKxGt'
-consumer_secret = 'bE2TIXle3yTTyYdaIkBOohYQGtzBglz9lXrZzaoo6FRPczTkm3'
-access_token = '362553785-kR1F4B9VZswfVv1Ut8ZRzym31mJo2WE7s8lqyH0l'
-access_secret = 'qbbo87lt3RvBZyWNOfAY1HdCDZxxfZ53msdFgy6XUluC6'
+consumer_key = ''
+consumer_secret = ''
+access_token = '-'
+access_secret = ''
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
